ca6a2defbd3a51cb60e3da64745863153d887a9f
vanch3d <nicolas.github@calques3d.org>
2006-11-14 18:49:43 +0000

    resource identifiers cleaned

--- resource.h
+++ resource.h
@@ -643,7 +643,7 @@
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        187
 #define _APS_NEXT_COMMAND_VALUE         32989
-#define _APS_NEXT_CONTROL_VALUE         1174
+#define _APS_NEXT_CONTROL_VALUE         1178
 #define _APS_NEXT_SYMED_VALUE           123
 #endif
 #endif
