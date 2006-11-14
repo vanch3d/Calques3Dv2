50ceff40201a856eeefdd39063f560434e1c090a
vanch3d <nicolas.github@calques3d.org>
2006-11-14 14:46:35 +0000

    Initial revision

---
+++ ProjectRCVersion.cpp
@@ -0,0 +1,27 @@
+// ProjectRCVersion.cpp: implementation of the CProjectRCVersion class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "calques3d.h"
+#include "ProjectRCVersion.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CProjectRCVersion::CProjectRCVersion()
+{
+
+}
+
+CProjectRCVersion::~CProjectRCVersion()
+{
+
+}
---
+++ ProjectRCVersion.h
@@ -0,0 +1,20 @@
+// ProjectRCVersion.h: interface for the CProjectRCVersion class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_PROJECTRCVERSION_H__E079ABD9_D702_4500_997C_D45A16A581E3__INCLUDED_)
+#define AFX_PROJECTRCVERSION_H__E079ABD9_D702_4500_997C_D45A16A581E3__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CProjectRCVersion  
+{
+public:
+	CProjectRCVersion();
+	virtual ~CProjectRCVersion();
+
+};
+
+#endif // !defined(AFX_PROJECTRCVERSION_H__E079ABD9_D702_4500_997C_D45A16A581E3__INCLUDED_)
