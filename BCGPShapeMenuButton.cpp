a5ac47d5a90512dc971a5d2363e6b64912fd339e
vanch3d <nicolas.github@calques3d.org>
2006-11-26 15:02:15 +0000

    Initial revision

---
+++ BCGPShapeMenuButton.cpp
@@ -0,0 +1,34 @@
+// BCGPShapeMenuButton.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "calques3d.h"
+#include "BCGPShapeMenuButton.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CBCGPShapeMenuButton
+
+CBCGPShapeMenuButton::CBCGPShapeMenuButton()
+{
+}
+
+CBCGPShapeMenuButton::~CBCGPShapeMenuButton()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CBCGPShapeMenuButton, CWnd)
+	//{{AFX_MSG_MAP(CBCGPShapeMenuButton)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CBCGPShapeMenuButton message handlers
---
+++ BCGPShapeMenuButton.h
@@ -0,0 +1,47 @@
+#if !defined(AFX_BCGPSHAPEMENUBUTTON_H__BCABDD04_841E_4B6A_8F61_E7EBE08212D4__INCLUDED_)
+#define AFX_BCGPSHAPEMENUBUTTON_H__BCABDD04_841E_4B6A_8F61_E7EBE08212D4__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// BCGPShapeMenuButton.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CBCGPShapeMenuButton window
+
+class CBCGPShapeMenuButton : public CWnd
+{
+// Construction
+public:
+	CBCGPShapeMenuButton();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CBCGPShapeMenuButton)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CBCGPShapeMenuButton();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CBCGPShapeMenuButton)
+		// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_BCGPSHAPEMENUBUTTON_H__BCABDD04_841E_4B6A_8F61_E7EBE08212D4__INCLUDED_)
