dcecefbb8cadb665e5933fd98ec1fb7a60ca8bd1
vanch3d <nicolas.github@calques3d.org>
2006-11-26 09:56:41 +0000

    Initial revision

---
+++ FormatToolBar.cpp
@@ -0,0 +1,34 @@
+// FormatToolBar.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "calques3d.h"
+#include "FormatToolBar.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CFormatToolBar
+
+CFormatToolBar::CFormatToolBar()
+{
+}
+
+CFormatToolBar::~CFormatToolBar()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CFormatToolBar, CWnd)
+	//{{AFX_MSG_MAP(CFormatToolBar)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+/////////////////////////////////////////////////////////////////////////////
+// CFormatToolBar message handlers
---
+++ FormatToolBar.h
@@ -0,0 +1,47 @@
+#if !defined(AFX_FORMATTOOLBAR_H__B7AF4660_2F25_4521_A10D_1D175146F444__INCLUDED_)
+#define AFX_FORMATTOOLBAR_H__B7AF4660_2F25_4521_A10D_1D175146F444__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// FormatToolBar.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CFormatToolBar window
+
+class CFormatToolBar : public CWnd
+{
+// Construction
+public:
+	CFormatToolBar();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CFormatToolBar)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CFormatToolBar();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CFormatToolBar)
+		// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_FORMATTOOLBAR_H__B7AF4660_2F25_4521_A10D_1D175146F444__INCLUDED_)
