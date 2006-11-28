2ca3c0abfc90d266f6e5383274ad9a4e4c1d47bd
vanch3d <nicolas.github@calques3d.org>
2006-11-28 12:20:46 +0000

    In-place edit for the labels and other texts

---
+++ objects/EditLabel.cpp
@@ -0,0 +1,33 @@
+// EditLabel.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "..\calques3d.h"
+#include "EditLabel.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CEditLabel
+
+CEditLabel::CEditLabel()
+{
+}
+
+CEditLabel::~CEditLabel()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CEditLabel, CEdit)
+	//{{AFX_MSG_MAP(CEditLabel)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CEditLabel message handlers
---
+++ objects/EditLabel.h
@@ -0,0 +1,48 @@
+#if !defined(AFX_EDITLABEL_H__3E5E88A0_39E4_4AFD_9A88_C125DDCCEF84__INCLUDED_)
+#define AFX_EDITLABEL_H__3E5E88A0_39E4_4AFD_9A88_C125DDCCEF84__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// EditLabel.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CEditLabel window
+
+class CEditLabel : public CEdit
+{
+// Construction
+public:
+	CEditLabel();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CEditLabel)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CEditLabel();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CEditLabel)
+		// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_EDITLABEL_H__3E5E88A0_39E4_4AFD_9A88_C125DDCCEF84__INCLUDED_)
