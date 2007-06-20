feedac60a53441adb930b8bec8425cfc1a3090c2
vanch3d <nicolas.github@calques3d.org>
2007-06-20 19:55:52 +0000

    Initial revision

---
+++ objects/Geom.h
@@ -0,0 +1,20 @@
+// Geom.h: interface for the CGeom class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_GEOM_H__5AB61FC8_358D_4CA2_AD60_7FC34CA97CAD__INCLUDED_)
+#define AFX_GEOM_H__5AB61FC8_358D_4CA2_AD60_7FC34CA97CAD__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CGeom  
+{
+public:
+	CGeom();
+	virtual ~CGeom();
+
+};
+
+#endif // !defined(AFX_GEOM_H__5AB61FC8_358D_4CA2_AD60_7FC34CA97CAD__INCLUDED_)
