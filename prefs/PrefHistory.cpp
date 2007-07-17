16fdb3705457a744c020cf0bf7e7adec0f066ae6
vanch3d <nicolas.github@calques3d.org>
2007-07-17 13:38:06 +0000

    - Fixed loading/saving preference data - A bit of documentation

--- prefs/PrefHistory.cpp
+++ prefs/PrefHistory.cpp
@@ -1,9 +1,31 @@
-// PrefHistory.cpp : implementation file
+//////////////////////////////////////////////////////////////////////
+// Calques 3D - a 3D Dynamic Geometry Learning Environment
+// Copyright (c) 1997-2007 Nicolas Van Labeke
+//////////////////////////////////////////////////////////////////////
+// This file is part of Calques 3D.
+// 
+// Calques 3D is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by 
+// the Free Software Foundation; either version 2 of the License, or 
+// (at your option) any later version.
+// 
+// Calques 3D is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License 
+// along with Calques 3D; if not, write to The Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+//////////////////////////////////////////////////////////////////////
+// PrefHistory.cpp: implementation file.
 //
+//////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
 #include "..\calques3d.h"
 #include "PrefHistory.h"
+#include "Prefs.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -14,12 +36,12 @@ static char THIS_FILE[] = __FILE__;
 /////////////////////////////////////////////////////////////////////////////
 // CPrefHistory property page
 
-IMPLEMENT_DYNCREATE(CPrefHistory, CPropertyPage)
+IMPLEMENT_DYNCREATE(CPrefHistory, CBCGPPropertyPage)
 
-CPrefHistory::CPrefHistory() : CPropertyPage(CPrefHistory::IDD)
+CPrefHistory::CPrefHistory() : CBCGPPropertyPage(CPrefHistory::IDD)
 {
 	//{{AFX_DATA_INIT(CPrefHistory)
-		// NOTE: the ClassWizard will add member initialization here
+	m_bShowHidden = TPref::THistory.bShowSelectHidden;
 	//}}AFX_DATA_INIT
 }
 
@@ -29,14 +51,18 @@ CPrefHistory::~CPrefHistory()
 
 void CPrefHistory::DoDataExchange(CDataExchange* pDX)
 {
-	CPropertyPage::DoDataExchange(pDX);
+	CBCGPPropertyPage::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(CPrefHistory)
-		// NOTE: the ClassWizard will add DDX and DDV calls here
+	DDX_Check(pDX, IDC_PHISTO_SHOWSEL, m_bShowHidden);
 	//}}AFX_DATA_MAP
+	if (pDX->m_bSaveAndValidate)
+	{
+		TPref::THistory.bShowSelectHidden = m_bShowHidden;
+	}
 }
 
 
-BEGIN_MESSAGE_MAP(CPrefHistory, CPropertyPage)
+BEGIN_MESSAGE_MAP(CPrefHistory, CBCGPPropertyPage)
 	//{{AFX_MSG_MAP(CPrefHistory)
 		// NOTE: the ClassWizard will add message map macros here
 	//}}AFX_MSG_MAP
--- prefs/PrefHistory.h
+++ prefs/PrefHistory.h
@@ -1,16 +1,40 @@
+//////////////////////////////////////////////////////////////////////
+// Calques 3D - a 3D Dynamic Geometry Learning Environment
+// Copyright (c) 1997-2007 Nicolas Van Labeke
+//////////////////////////////////////////////////////////////////////
+// This file is part of Calques 3D.
+// 
+// Calques 3D is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by 
+// the Free Software Foundation; either version 2 of the License, or 
+// (at your option) any later version.
+// 
+// Calques 3D is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License 
+// along with Calques 3D; if not, write to The Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+//////////////////////////////////////////////////////////////////////
+/// @file PrefHistory.h
+/// @brief interface of the CPrefHistory class.
+///
+///
+//////////////////////////////////////////////////////////////////////
 #if !defined(AFX_PREFHISTORY_H__AB60439D_5522_41B8_8745_ACB8700E1C52__INCLUDED_)
 #define AFX_PREFHISTORY_H__AB60439D_5522_41B8_8745_ACB8700E1C52__INCLUDED_
 
 #if _MSC_VER > 1000
-#pragma once
+	#pragma once
 #endif // _MSC_VER > 1000
-// PrefHistory.h : header file
-//
 
 /////////////////////////////////////////////////////////////////////////////
-// CPrefHistory dialog
-
-class CPrefHistory : public CPropertyPage
+/// CPrefHistory dialog
+///
+/////////////////////////////////////////////////////////////////////////////
+class CPrefHistory : public CBCGPPropertyPage
 {
 	DECLARE_DYNCREATE(CPrefHistory)
 
@@ -22,8 +46,7 @@ public:
 // Dialog Data
 	//{{AFX_DATA(CPrefHistory)
 	enum { IDD = IDD_PREF_HISTORY_PAGE };
-		// NOTE - ClassWizard will add data members here.
-		//    DO NOT EDIT what you see in these blocks of generated code !
+	BOOL	m_bShowHidden;
 	//}}AFX_DATA
 
 
