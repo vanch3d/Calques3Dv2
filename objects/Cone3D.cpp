ab4856fe4042c0dac38e3507fc81a8add8b74c86
vanch3d <nicolas.github@calques3d.org>
2007-07-19 18:47:28 +0000

    Redefinition of the cone object

---
+++ objects/Cone3D.cpp
@@ -0,0 +1,27 @@
+// Cone3D.cpp: implementation of the CCone3D class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "calques3d.h"
+#include "Cone3D.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CCone3D::CCone3D()
+{
+
+}
+
+CCone3D::~CCone3D()
+{
+
+}
---
+++ objects/Cone3D.h
@@ -0,0 +1,20 @@
+// Cone3D.h: interface for the CCone3D class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_CONE3D_H__A3934306_BBF1_4C76_B690_4826B9B8A7F0__INCLUDED_)
+#define AFX_CONE3D_H__A3934306_BBF1_4C76_B690_4826B9B8A7F0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CCone3D  
+{
+public:
+	CCone3D();
+	virtual ~CCone3D();
+
+};
+
+#endif // !defined(AFX_CONE3D_H__A3934306_BBF1_4C76_B690_4826B9B8A7F0__INCLUDED_)
