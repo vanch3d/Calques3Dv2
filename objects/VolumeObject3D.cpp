18d13d5fa7823bb40cf8f691ca4f0b2fc5ae3d74
vanch3d <nicolas.github@calques3d.org>
2007-07-19 18:04:50 +0000

    Added abstract class for Volume Objects

---
+++ objects/VolumeObject3D.cpp
@@ -0,0 +1,27 @@
+// VolumeObject3D.cpp: implementation of the CVolumeObject3D class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "calques3d.h"
+#include "VolumeObject3D.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CVolumeObject3D::CVolumeObject3D()
+{
+
+}
+
+CVolumeObject3D::~CVolumeObject3D()
+{
+
+}
---
+++ objects/VolumeObject3D.h
@@ -0,0 +1,20 @@
+// VolumeObject3D.h: interface for the CVolumeObject3D class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_VOLUMEOBJECT3D_H__8E2F48C7_ECE8_4B81_BD3A_64E06A25ACDE__INCLUDED_)
+#define AFX_VOLUMEOBJECT3D_H__8E2F48C7_ECE8_4B81_BD3A_64E06A25ACDE__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CVolumeObject3D  
+{
+public:
+	CVolumeObject3D();
+	virtual ~CVolumeObject3D();
+
+};
+
+#endif // !defined(AFX_VOLUMEOBJECT3D_H__8E2F48C7_ECE8_4B81_BD3A_64E06A25ACDE__INCLUDED_)
