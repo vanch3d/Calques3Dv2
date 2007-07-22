4934333bbd19f9c90718d9dbffee5afb499a911b
vanch3d <nicolas.github@calques3d.org>
2007-07-22 09:39:21 +0000

    Fixed missing methods

--- objects/VolumeObject3D.cpp
+++ objects/VolumeObject3D.cpp
@@ -1,9 +1,29 @@
+//////////////////////////////////////////////////////////////////////
+// Calques 3D - a 3D Dynamic Geometry Learning Environment
+// Copyright (c) 1997-2007 Nicolas Van Labeke
+//////////////////////////////////////////////////////////////////////
+// This file is part of Calques 3D.
+// 
+// Calques 3D is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by 
+// the Free Software Foundation; either version 2 of the License, or 
+// (at your option) any later version.
+// 
+// Calques 3D is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License 
+// along with Calques 3D; if not, write to The Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+//////////////////////////////////////////////////////////////////////
 // VolumeObject3D.cpp: implementation of the CVolumeObject3D class.
 //
 //////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
-#include "calques3d.h"
+#include "..\Calques3D.h"
 #include "VolumeObject3D.h"
 
 #ifdef _DEBUG
@@ -15,13 +35,28 @@ static char THIS_FILE[]=__FILE__;
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
+IMPLEMENT_SERIAL(CVolumeObject3D, CObject3D, VERSIONABLE_SCHEMA | 1)
 
-CVolumeObject3D::CVolumeObject3D()
+CVolumeObject3D::CVolumeObject3D() : CObject3D() 
 {
+}
 
+CVolumeObject3D::CVolumeObject3D(const CObject3D & src): CObject3D(src)
+{
 }
 
 CVolumeObject3D::~CVolumeObject3D()
 {
-
 }
+
+FCoord CVolumeObject3D::GetVolume()
+{
+	return 0.;
+}
+
+FCoord CVolumeObject3D::GetSurface()
+{
+	return 0.;
+}
+
+
--- objects/VolumeObject3D.h
+++ objects/VolumeObject3D.h
@@ -1,20 +1,52 @@
-// VolumeObject3D.h: interface for the CVolumeObject3D class.
-//
 //////////////////////////////////////////////////////////////////////
-
+// Calques 3D - a 3D Dynamic Geometry Learning Environment
+// Copyright (c) 1997-2007 Nicolas Van Labeke
+//////////////////////////////////////////////////////////////////////
+// This file is part of Calques 3D.
+// 
+// Calques 3D is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by 
+// the Free Software Foundation; either version 2 of the License, or 
+// (at your option) any later version.
+// 
+// Calques 3D is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License 
+// along with Calques 3D; if not, write to The Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
+//////////////////////////////////////////////////////////////////////
+/// @file VolumeObject3D.h
+/// @brief interface of the CVolumeObject3D class.
+///
+///
+//////////////////////////////////////////////////////////////////////
 #if !defined(AFX_VOLUMEOBJECT3D_H__8E2F48C7_ECE8_4B81_BD3A_64E06A25ACDE__INCLUDED_)
 #define AFX_VOLUMEOBJECT3D_H__8E2F48C7_ECE8_4B81_BD3A_64E06A25ACDE__INCLUDED_
 
 #if _MSC_VER > 1000
-#pragma once
+	#pragma once
 #endif // _MSC_VER > 1000
 
-class CVolumeObject3D  
+#include "Object3D.h"
+
+//////////////////////////////////////////////////////////////////////
+/// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+class CVolumeObject3D : public CObject3D 
 {
 public:
+	DECLARE_SERIAL(CVolumeObject3D);
+	
 	CVolumeObject3D();
+	CVolumeObject3D(const CObject3D & );
 	virtual ~CVolumeObject3D();
 
+public:
+	virtual FCoord GetVolume();
+	virtual FCoord GetSurface();
 };
 
 #endif // !defined(AFX_VOLUMEOBJECT3D_H__8E2F48C7_ECE8_4B81_BD3A_64E06A25ACDE__INCLUDED_)
