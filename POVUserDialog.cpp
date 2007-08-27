b3da1162c871a985c1445831e30d145ef38d0471
vanch3d <nicolas.github@calques3d.org>
2007-08-27 15:21:22 +0000

    updated dialog and list for the user-defined PoV

--- POVUserDialog.cpp
+++ POVUserDialog.cpp
@@ -4,6 +4,8 @@
 #include "stdafx.h"
 #include "calques3d.h"
 #include "POVUserDialog.h"
+#include "prefs\Prefs.h"
+#include "prefs\POVTool.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -13,13 +15,120 @@ static char THIS_FILE[] = __FILE__;
 
 /////////////////////////////////////////////////////////////////////////////
 // CPOVUserDialog dialog
+static const int iListId = 1;
+
+BEGIN_MESSAGE_MAP(CPOVList,CBCGPEditListBox)
+	//ON_NOTIFY(LVN_KEYDOWN, iListId, OnKeyDown)
+	ON_NOTIFY(NM_DBLCLK, iListId, OnDblclkList)
+	//ON_NOTIFY(LVN_GETDISPINFO, iListId, OnGetdispinfo)
+	//ON_NOTIFY(LVN_ENDLABELEDIT, iListId, OnEndLabelEdit)
+	//ON_NOTIFY(LVN_ITEMCHANGED, iListId, OnItemChanged)
+END_MESSAGE_MAP()
+
+void CPOVList::OnDblclkList(NMHDR* /*pNMHDR*/, LRESULT* pResult) 
+{
+	*pResult = 0;
+
+	int iSelItem = GetSelItem ();
+
+	if ((m_uiStandardBtns & BGCEDITLISTBOX_BTN_NEW) && iSelItem == -1)
+	{
+		CreateNewItem ();
+		return;
+	}
+
+	if (iSelItem > 0)
+	{
+		EditItem (iSelItem);
+	}
+}
+
+BOOL CPOVList::OnBeforeRemoveItem (int iItem)	
+{	
+	CPOVUserTool* pSelTool = (CPOVUserTool*) GetItemData (iItem);
+	if (pSelTool)
+	{
+		for (POSITION pos = m_pParent->m_pObjList->GetHeadPosition (); pos != NULL;)
+		{
+			POSITION locpos = pos;
+			CPOVUserTool *pObj = m_pParent->m_pObjList->GetNext (pos);
+			if (!pObj) continue;
+			if (pObj==pSelTool) 
+			{
+				m_pParent->m_pObjList->RemoveAt(locpos);
+				break;
+			}
+		}
+		delete pSelTool;
+		SetItemData(iItem,(DWORD)NULL);
+	}
+	return TRUE;	
+}
+	
+void CPOVList::OnAfterAddItem (int iItem) 
+{
+	CString str = GetItemText(iItem);
+	CPOVUserTool *tool = new CPOVUserTool();
+	tool->m_bCanRemove = TRUE;
+	tool->m_strLabel = str;
+	tool->m_projParam = m_pParent->m_projParam;
+	SetItemData(iItem,(DWORD)tool);
+	m_pParent->m_pObjList->AddHead(tool);
+	m_pParent->ShowProperty(tool);
+
+}
+
+void CPOVList::OnSelectionChanged ()
+{
+	int iSelItem = GetSelItem ();
+
+	int nb = GetButtonsCount();
+	int nbitem = GetCount();
+	EnableButton(0,iSelItem>1);
+	for (int i=1;i<nb-1;i++)
+	{
+		EnableButton(i,(BOOL)iSelItem);
+	}
+	EnableButton(nb-1,nbitem<5);
+
+	CPOVUserTool* pSelTool = (iSelItem < 0) ? 
+		NULL : (CPOVUserTool*) GetItemData (iSelItem);
+	m_pParent->ShowProperty(pSelTool);
+}
+
+void CPOVList::OnAfterMoveItemUp (int iItem)
+{
+	OnSelectionChanged ();
+			SetRedraw ();
+}
+
+void CPOVList::OnAfterMoveItemDown (int /*iItem*/)
+{
+	OnSelectionChanged ();
+			SetRedraw ();
+}
+
+BOOL CPOVList::EditItem (int iIndex)
+{
+	BOOL bRes = FALSE;
+	if (iIndex)
+	{
+		bRes = CBCGPEditListBox::EditItem (iIndex);
+	}
+	return bRes;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// CPOVUserDialog dialog
 
 
 CPOVUserDialog::CPOVUserDialog(CWnd* pParent /*=NULL*/)
-	: CDialog(CPOVUserDialog::IDD, pParent)
+	:	CDialog(CPOVUserDialog::IDD, pParent),
+		m_wndPOVListBox(this)
 {
+	m_pObjList = NULL;
 	//{{AFX_DATA_INIT(CPOVUserDialog)
-		// NOTE: the ClassWizard will add member initialization here
+	m_strProp = _T("");
 	//}}AFX_DATA_INIT
 }
 
@@ -28,16 +137,89 @@ void CPOVUserDialog::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(CPOVUserDialog)
-		// NOTE: the ClassWizard will add DDX and DDV calls here
+	DDX_Control(pDX, IDC_POV_LIST, m_wndPOVListBox);
+	DDX_Text(pDX, IDC_POV_INFO, m_strProp);
 	//}}AFX_DATA_MAP
+
+	if (pDX->m_bSaveAndValidate)
+	{
+		int nb = m_wndPOVListBox.GetCount();
+		m_pObjList->RemoveAll();
+		for (int i=0;i<nb;i++)
+		{
+			CPOVUserTool *pObj = (CPOVUserTool *)m_wndPOVListBox.GetItemData(i);
+			CString str = m_wndPOVListBox.GetItemText(i);
+			if (!pObj) continue;
+			pObj->m_strLabel = str;
+			m_pObjList->AddTail(pObj);
+		}
+	}
 }
 
 
 BEGIN_MESSAGE_MAP(CPOVUserDialog, CDialog)
 	//{{AFX_MSG_MAP(CPOVUserDialog)
-		// NOTE: the ClassWizard will add message map macros here
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
 // CPOVUserDialog message handlers
+
+BOOL CPOVUserDialog::OnInitDialog() 
+{
+
+	CDialog::OnInitDialog();
+	
+	// TODO: Add extra initialization here
+	UINT nStdBtn = BGCEDITLISTBOX_BTN_UP | BGCEDITLISTBOX_BTN_DOWN;
+	m_wndPOVListBox.SetStandardButtons (nStdBtn);
+	m_wndPOVListBox.AddButton(IDB_LIST_DELITEM,"Remove this point of view",VK_DELETE,0,BGCEDITLISTBOX_BTN_DELETE_ID);
+	m_wndPOVListBox.AddButton(IDB_LIST_ADDITEM,"Save the current point of view",VK_INSERT,0,BGCEDITLISTBOX_BTN_NEW_ID);
+	nStdBtn |= BGCEDITLISTBOX_BTN_NEW | BGCEDITLISTBOX_BTN_DELETE;
+	m_wndPOVListBox.SetStandardBtns(nStdBtn);
+	//m_wndPOVListBox.EnableBrowseButton ();
+	m_wndPOVListBox.SetGrayDisabledButtons();
+	
+	//------------
+	// Fill tools:
+	//------------
+	if (m_pObjList==NULL) return FALSE;
+// 	for(POSITION pos = TPref::TMacroList.GetHeadPosition(); pos != NULL; )
+// 	{
+// 		CMacroUserTool* pTool = (CMacroUserTool*)TPref::TMacroList.GetNext( pos );
+// 		if (pTool)
+// 			m_wndMacroListBox.AddItem (pTool->m_strLabel, (DWORD) pTool);
+// 	}
+
+	for (POSITION pos = m_pObjList->GetHeadPosition (); pos != NULL;)
+	{
+		CPOVUserTool *pObj = m_pObjList->GetNext (pos);
+		if (pObj) m_wndPOVListBox.AddItem(pObj->m_strLabel,(DWORD)pObj);
+	}
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	              // EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CPOVUserDialog::OnOK()
+{
+	CDialog::OnOK();
+}
+
+void CPOVUserDialog::OnCancel()
+{
+	UpdateData(TRUE);
+	CDialog::OnCancel();
+}
+
+void CPOVUserDialog::ShowProperty(CPOVUserTool* prop)
+{
+	m_strProp = _T("");
+	if (prop!=NULL)
+	{
+		SProjection proj = prop->m_projParam;
+		m_strProp.Format(_T("theta=%f , phi=%f"),proj.theta,proj.phi);
+	}
+
+	UpdateData(FALSE);
+}
--- POVUserDialog.h
+++ POVUserDialog.h
@@ -7,19 +7,52 @@
 // POVUserDialog.h : header file
 //
 
+#include "objects/Vector4.h"
+
 /////////////////////////////////////////////////////////////////////////////
 // CPOVUserDialog dialog
+
+class CPOVUserDialog;
+class CxPOVObjectSet;
+class CPOVUserTool;
+/////////////////////////////////////////////////////////////////////////////
+/// CPOVList
+///
+/////////////////////////////////////////////////////////////////////////////
+class CPOVList : public CBCGPEditListBox
+{
+public:
+	CPOVUserDialog* m_pParent;
+
+	CPOVList(CPOVUserDialog* pParent=NULL) : m_pParent (pParent) {};
+
+	virtual void OnSelectionChanged ();
+	virtual BOOL OnBeforeRemoveItem (int /*iItem*/);
+	virtual void OnAfterAddItem (int /*iItem*/);
+	virtual void OnAfterMoveItemUp (int /*iItem*/);
+	virtual void OnAfterMoveItemDown (int /*iItem*/);
+	virtual BOOL EditItem (int iIndex);
+
+	UINT GetStandardBtns() { return m_uiStandardBtns;};
+	void SetStandardBtns(UINT stdBtns) { m_uiStandardBtns = stdBtns;};
+
+protected:
+	afx_msg void OnDblclkList (NMHDR* pNMHDR, LRESULT* pResult);
+	DECLARE_MESSAGE_MAP()
+};
 
 class CPOVUserDialog : public CDialog
 {
 // Construction
 public:
 	CPOVUserDialog(CWnd* pParent = NULL);   // standard constructor
-
+	CxPOVObjectSet* m_pObjList;
+	SProjection 	m_projParam;
 // Dialog Data
 	//{{AFX_DATA(CPOVUserDialog)
 	enum { IDD = IDD_POV_DLG };
-		// NOTE: the ClassWizard will add data members here
+	CPOVList		m_wndPOVListBox;		///< The control for the macro item list
+	CString	m_strProp;
 	//}}AFX_DATA
 
 
@@ -30,12 +63,19 @@ public:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 	//}}AFX_VIRTUAL
 
+public:
+	void SetPOVList(CxPOVObjectSet* mylist) {m_pObjList = mylist;};
+	void SetProjParam(SProjection myParam) {m_projParam = myParam;};
+	void ShowProperty(CPOVUserTool* prop);
 // Implementation
 protected:
+	virtual void OnOK();
+	virtual void OnCancel();
+
 
 	// Generated message map functions
 	//{{AFX_MSG(CPOVUserDialog)
-		// NOTE: the ClassWizard will add member functions here
+	virtual BOOL OnInitDialog();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
