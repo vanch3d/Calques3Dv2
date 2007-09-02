718e09bb537eafb1a0769b034c2ebb50210cb7e7
vanch3d <nicolas.github@calques3d.org>
2007-09-02 08:52:39 +0000

    added class for conic sections

---
+++ objects/ConicSection3D.cpp
@@ -0,0 +1,27 @@
+// ConicSection3D.cpp: implementation of the CConicSection3D class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "calques3d.h"
+#include "ConicSection3D.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CConicSection3D::CConicSection3D()
+{
+
+}
+
+CConicSection3D::~CConicSection3D()
+{
+
+}
---
+++ objects/ConicSection3D.h
@@ -0,0 +1,22 @@
+// ConicSection3D.h: interface for the CConicSection3D class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_CONICSECTION3D_H__3BBB8B3E_CC71_4107_9553_FB9C1B829F8B__INCLUDED_)
+#define AFX_CONICSECTION3D_H__3BBB8B3E_CC71_4107_9553_FB9C1B829F8B__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "Object3D.h"
+
+class CConicSection3D : public CObject3D  
+{
+public:
+	CConicSection3D();
+	virtual ~CConicSection3D();
+
+};
+
+#endif // !defined(AFX_CONICSECTION3D_H__3BBB8B3E_CC71_4107_9553_FB9C1B829F8B__INCLUDED_)
